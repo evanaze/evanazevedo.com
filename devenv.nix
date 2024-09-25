9e486e9c55f8ff25b294c33bb22cbd46bd8aa50f
Evan Azevedo <me@evanazevedo.com>
2024-09-25 11:44:33 -0600

    main using gomod2nix

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,7 @@
   inputs,
   ...
 }: {
-  packages = [pkgs.gomod2nix pkgs.hugo pkgs.git];
+  packages = [pkgs.gomod2nix pkgs.hugo];
 
   # https://devenv.sh/languages/
   languages.go.enable = true;
@@ -16,28 +16,22 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.serve.exec = ''
+  scripts.hserve.exec = ''
     hugo-obsidian -input=content -output=assets/indices -index -root=.
-    hugo server --enableGitInfo --minify --bind=$(or $(HUGO_BIND),0.0.0.0) --baseURL=$(or $(HUGO_BASEURL),http://localhost) --port=$(or $(HUGO_PORT),1313) --appendPort=$(or $(HUGO_APPENDPORT),true) --liveReloadPort=$(or $(HUGO_LIVERELOADPORT),-1)
-  '';
+    ${pkgs.hugo} server --enableGitInfo --minify '';
 
   # https://devenv.sh/tests/
 
   # https://devenv.sh/pre-commit-hooks/
-  pre-commit.hooks = {
-    govet = {
-      enable = true;
-      pass_filenames = false;
-    };
-    gotest.enable = true;
-    golangci-lint = {
-      enable = true;
-      pass_filenames = false;
-    };
-  };
-
-  outputs = let
-    name = "evanazevedo.com";
-    version = "1.0.0";
-  in {app = import ./default.nix {inherit pkgs name version;};};
+  # pre-commit.hooks = {
+  #   govet = {
+  #     enable = true;
+  #     pass_filenames = false;
+  #   };
+  #   gotest.enable = true;
+  #   golangci-lint = {
+  #     enable = true;
+  #     pass_filenames = false;
+  #   };
+  # };
 }
