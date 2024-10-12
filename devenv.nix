0f2172629d7b3e8e5c85281220df04df157c10ce
Evan Azevedo <me@evanazevedo.com>
2024-10-11 18:48:06 -0600

    v2 local

--- devenv.nix
+++ devenv.nix
@@ -1,44 +1,23 @@
-{
-  pkgs,
-  lib,
-  config,
-  inputs,
-  ...
-}: {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
+{pkgs, ...}: {
   # https://devenv.sh/packages/
   packages = with pkgs; [
     hugo
     nodejs
+    tailwindcss
   ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
 
-  # https://devenv.sh/processes/
-  processes.hugo-build.exec = "hugo server -D";
-
   # https://devenv.sh/services/
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.build.exec = ''
-    echo hello from $GREET
-  '';
+  scripts.build.exec = "hugo server -D";
 
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
+  processes = {
+    tw.exec = "tailwindcss -i ./assets/input.css -o ./assets/output.css --watch";
+  };
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
